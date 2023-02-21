40a3ec36994c1757d36b7d8e173e25a8de91fbed
Auryn Macmillan <hi@auryn.xyz>
2023-02-21 14:43:53 +1100

    chore: remove references to greeter

--- test/types.ts
+++ test/types.ts
@@ -1,12 +1,18 @@
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address"
 
-import type { Greeter } from "../types/Greeter"
+import type { GiriGiriBashi } from "../types/contracts/GiriGiriBashi"
+import type { Hashi } from "../types/contracts/Hashi"
 
 type Fixture<T> = () => Promise<T>
 
 declare module "mocha" {
   export interface Context {
-    greeter: Greeter
+    hashi: Hashi
+    loadFixture: <T>(fixture: Fixture<T>) => Promise<T>
+    signers: Signers
+  }
+  export interface Context {
+    giriGiriBashi: GiriGiriBashi
     loadFixture: <T>(fixture: Fixture<T>) => Promise<T>
     signers: Signers
   }
