324238b9751b4dd402ee978d64a96f3896960580
Rahul Sethuram <rahul@sethuram.com>
2023-03-05 17:36:54 -0700

    feat: add Connext bridge adapter

---
+++ contracts/adapters/Connext/ConnextAdapter.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.17;
+
+import "@connext/interfaces/core/IConnext.sol";
+import "@connext/interfaces/core/IXReceiver.sol";
+import "../IOracleAdapter.sol";
+
+contract ConnextAdapter is IXReceiver {
+    bytes32 public headerReporter;
+    mapping(uint256 => mapping(uint256 => bytes32)) public headers;
+
+    event HeaderStored(uint256 indexed blockNumber, bytes32 indexed blockHeader);
+
+    error InvalidInputs();
+    error InvalidSource(address _originSender, uint32 _origin);
+
+    mapping(uint32 => uint16) domainToChainId;
+    mapping(uint32 => address) domainToSource;
+    mapping(uint32 => address) domainToConnext;
+
+    constructor(
+        address[] memory _connexts,
+        uint32[] memory _originDomains,
+        uint16[] memory _originChainIds,
+        address[] memory _sources,
+        bytes32 _headerReporter
+    ) {
+        headerReporter = _headerReporter;
+        if (
+            _originDomains.length != _connexts.length ||
+            _originDomains.length != _originChainIds.length ||
+            _originDomains.length != _sources.length
+        ) {
+            revert InvalidInputs();
+        }
+        for (uint256 i = 0; i < _originDomains.length; i++) {
+            domainToChainId[_originDomains[i]] = _originChainIds[i];
+            domainToSource[_originDomains[i]] = _sources[i];
+            domainToConnext[_originDomains[i]] = address(_connexts[i]);
+        }
+    }
+
+    /** @notice A modifier for authenticated calls.
+     * This is an important security consideration. If the target contract
+     * function should be authenticated, it must check three things:
+     *    1) The originating call comes from the expected origin domain.
+     *    2) The originating call comes from the expected source contract.
+     *    3) The call to this contract comes from Connext.
+     */
+    modifier onlySource(address _originSender, uint32 _origin) {
+        if (domainToSource[_origin] != _originSender || msg.sender != domainToConnext[_origin]) {
+            revert InvalidSource(_originSender, _origin);
+        }
+        _;
+    }
+
+    /// @dev Stores the block header for a given block.
+    /// @param _transferId Unique Identifier for the transfer.
+    /// @param _amount Amount of tokens transferred (0 in this case).
+    /// @param _asset Asset transferred (irrelevant).
+    /// @param _originSender Address which originated the call on the origin chain.
+    /// @param _origin Domain for the origin chain.
+    /// @param _callData decodable calldata passed through the Connext bridge.
+    /// @notice Only callable by `connext` with a message passed from `headerReporter.
+    function xReceive(
+        bytes32 _transferId,
+        uint256 _amount,
+        address _asset,
+        address _originSender,
+        uint32 _origin,
+        bytes memory _callData
+    ) external onlySource(_originSender, _origin) returns (bytes memory) {
+        // Unpack the _callData
+        (uint256 blockNumber, bytes32 newBlockHeader) = abi.decode(_callData, (uint256, bytes32));
+        bytes32 currentBlockHeader = headers[uint256(domainToChainId[_origin])][blockNumber];
+        if (currentBlockHeader != newBlockHeader) {
+            headers[uint256(domainToChainId[_origin])][blockNumber] = newBlockHeader;
+            emit HeaderStored(blockNumber, newBlockHeader);
+        }
+    }
+
+    /// @dev Returns the block header for a given block, as reported by the Wormhole.
+    /// @param blockNumber Identifier for the block to query.
+    /// @return blockHeader Bytes32 block header reported by the oracle for the given block on the given chain.
+    /// @notice MUST return bytes32(0) if the oracle has not yet reported a header for the given block.
+    function getHeaderFromOracle(uint256 chainId, uint256 blockNumber) external view returns (bytes32 blockHeader) {
+        blockHeader = headers[chainId][blockNumber];
+    }
+}
