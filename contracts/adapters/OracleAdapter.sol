73d909bd37a48c7a0c7c76e9ceb89657d34987d6
Laurent De Marez <laurent@demarez.org>
2023-03-17 18:06:44 +0100

    feat: add oracle adapter base contract

---
+++ contracts/adapters/OracleAdapter.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.17;
+
+import "./interfaces/IOracleAdapter.sol";
+
+abstract contract OracleAdapter is IOracleAdapter {
+    mapping(uint256 => mapping(uint256 => bytes32)) public hashes;
+
+    /// @dev Returns the hash for a given ID, as reported by the oracle.
+    /// @param domain Identifier for the domain to query.
+    /// @param id Identifier for the ID to query.
+    /// @return hash Bytes32 hash reported by the oracle for the given ID on the given domain.
+    /// @notice MUST return bytes32(0) if the oracle has not yet reported a hash for the given ID.
+    function getHashFromOracle(uint256 domain, uint256 id) external view returns (bytes32 hash) {
+        hash = hashes[domain][id];
+    }
+
+    function _storeHash(uint256 domain, uint256 id, bytes32 hash) internal {
+        bytes32 currentHash = hashes[domain][id];
+        if (currentHash != hash) {
+            hashes[domain][id] = hash;
+            emit HashStored(id, hash);
+        }
+    }
+}
--- contracts/adapters/Wormhole/WormholeAdapter.sol
+++ contracts/adapters/Wormhole/WormholeAdapter.sol
@@ -2,14 +2,11 @@
 pragma solidity ^0.8.17;
 
 import "./IWormhole.sol";
-import "../interfaces/IOracleAdapter.sol";
+import "../OracleAdapter.sol";
 
-contract WormholeAdapter {
+contract WormholeAdapter is OracleAdapter {
     IWormhole public wormhole;
     bytes32 public headerReporter;
-    mapping(uint256 => mapping(uint256 => bytes32)) public headers;
-
-    event HeaderStored(uint256 indexed blockNumber, bytes32 indexed blockHeader);
 
     error InvalidMessage(address emitter, VM vm, string reason);
     error InvalidChainId(address emitter, uint16 chainId);
@@ -26,23 +23,11 @@ contract WormholeAdapter {
     /// @param vm Structured data reflecting the content of the Wormhole Verified Action Approval.
     /// @notice Only callable by `wormhole` with a message passed from `headerReporter.
     function storeBlockHeader(uint256 blockNumber, uint16 chainId, VM memory vm) public {
-        bytes32 currentBlockHeader = headers[uint256(chainId)][blockNumber];
         (VM memory _vm, bool valid, string memory reason) = wormhole.parseAndVerifyVM(abi.encode(vm));
         if (!valid) revert InvalidMessage(address(this), vm, reason);
         if (_vm.emitterChainId != chainId) revert InvalidChainId(address(this), _vm.emitterChainId);
         if (_vm.emitterAddress != headerReporter) revert InvalidReporter(address(this), _vm.emitterAddress);
-        bytes32 newBlockHeader = abi.decode(_vm.payload, (bytes32));
-        if (currentBlockHeader != newBlockHeader) {
-            headers[uint256(chainId)][blockNumber] = newBlockHeader;
-            emit HeaderStored(blockNumber, newBlockHeader);
-        }
-    }
-
-    /// @dev Returns the block header for a given block, as reported by the Wormhole.
-    /// @param blockNumber Identifier for the block to query.
-    /// @return blockHeader Bytes32 block header reported by the oracle for the given block on the given chain.
-    /// @notice MUST return bytes32(0) if the oracle has not yet reported a header for the given block.
-    function getHeaderFromOracle(uint256 chainId, uint256 blockNumber) external view returns (bytes32 blockHeader) {
-        blockHeader = headers[chainId][blockNumber];
+        bytes32 newHash = abi.decode(_vm.payload, (bytes32));
+        _storeHash(uint256(chainId), blockNumber, newHash);
     }
 }
