6a3663e0ea8525c52fa4a53a250141ae88f4ad2c
Laurent De Marez <laurent@demarez.org>
2023-03-17 13:32:53 +0100

    chore: add test cases for proveAncestralBlocks

--- test/adapters/02_OracleAdapter.spec.ts
+++ test/adapters/02_OracleAdapter.spec.ts
@@ -1,9 +1,10 @@
 import { expect } from "chai"
-import { hexlify } from "ethers/lib/utils"
+import { RLP, hexlify } from "ethers/lib/utils"
 import { ethers, network } from "hardhat"
 
 const CHAIN_ID = 1
 
+// Use evm_mine instead of hardhat_mine because hardhat_mine doesn't provide valid blocks
 const mine = async (n: number) => await Promise.all([...Array(n)].map(() => network.provider.send("evm_mine")))
 
 const emptyHexlify = (value: string) => {
@@ -30,7 +31,15 @@ const blockRLP = (block: any) => {
     block.nonce,
     block.baseFeePerGas,
   ]
-  return ethers.utils.RLP.encode(values.map(emptyHexlify))
+  return RLP.encode(values.map(emptyHexlify))
+}
+
+const getBlock = async (blockNumber: number) => {
+  const block = await ethers.provider.send("eth_getBlockByNumber", [hexlify(blockNumber), false])
+  return {
+    ...block,
+    blockNumber,
+  }
 }
 
 const setup = async () => {
@@ -40,23 +49,26 @@ const setup = async () => {
   const oracleAdapter = await OracleAdapter.deploy()
   await mine(120)
   const reportedBlockNumbers = [100, 110]
-  const reportedBlockHeaders = await Promise.all(
-    reportedBlockNumbers.map(async (blockNumber) => (await ethers.provider.getBlock(blockNumber)).hash),
+  const unreportedBlockNumbers = [109, 108, 107]
+  const blocks = await Promise.all(
+    reportedBlockNumbers.concat(unreportedBlockNumbers).map(async (blockNumber) => await getBlock(blockNumber)),
   )
+  const reportedBlockHeaders = blocks
+    .filter((block) => reportedBlockNumbers.includes(block.blockNumber))
+    .map((block) => block.hash)
+
   await oracleAdapter.setBlockHeaders(CHAIN_ID, reportedBlockNumbers, reportedBlockHeaders)
   return {
     wallet,
     oracleAdapter,
     reportedBlockNumbers,
     reportedBlockHeaders,
+    unreportedBlockNumbers,
+    blocks,
   }
 }
 
-const getBlock = async (blockNumber: number) => {
-  return await ethers.provider.send("eth_getBlockByNumber", [hexlify(blockNumber), false])
-}
-
-describe.only("HeaderStorage", function () {
+describe("HeaderStorage", function () {
   describe("Deploy", function () {
     it("Successfully deploys contract", async function () {
       const { oracleAdapter } = await setup()
@@ -64,16 +76,63 @@ describe.only("HeaderStorage", function () {
     })
   })
 
-  describe("proveHeaders()", function () {
-    it("Verifies a unreported block header", async function () {
+  describe("proveAncestralHeaders()", function () {
+    it("Adds ancestral block headers of proven blocks", async function () {
+      const { oracleAdapter, blocks } = await setup()
+      const headerProofs = blocks.map((block) => blockRLP(block))
+
+      // All blocks should have been proven and their ancestral headers stored
+      for (const block of blocks) {
+        await expect(oracleAdapter.proveAncestralHeaders(CHAIN_ID, headerProofs))
+          .to.emit(oracleAdapter, "HeaderStored")
+          .withArgs(block.blockNumber - 1, block.parentHash)
+      }
+    })
+
+    it("Reverts if given unknown blocks", async function () {
+      const { oracleAdapter, blocks, unreportedBlockNumbers } = await setup()
+      const unreportedBlockNumber = unreportedBlockNumbers[0]
+      const unreportedBlock = blocks.find((block) => block.blockNumber === unreportedBlockNumber)
+      const headerProofs = [blockRLP(unreportedBlock)]
+
+      await expect(oracleAdapter.proveAncestralHeaders(CHAIN_ID, headerProofs))
+        .to.revertedWithCustomError(oracleAdapter, "InvalidBlockHeaderProof")
+        .withArgs(unreportedBlockNumber, unreportedBlock.hash)
+    })
+
+    it("Reverts if block proof is invalid RLP", async function () {
       const { oracleAdapter } = await setup()
+      const invalidRLP = ["0xa0000000"]
 
-      const bockNumbers = [110, 109, 108, 107]
-      const blocks = await Promise.all(bockNumbers.map(async (blockNumber) => await getBlock(blockNumber)))
-      const headerContents = blocks.map((block) => blockRLP(block))
-      const headers = blocks.map((block) => block.hash)
+      // Invalid block proof
+      await expect(oracleAdapter.proveAncestralHeaders(CHAIN_ID, invalidRLP)).to.revertedWithCustomError(
+        oracleAdapter,
+        "InvalidBlockHeaderProofRLP",
+      )
+    })
 
-      await oracleAdapter.proveHeaders(CHAIN_ID, headers, headerContents)
+    it("Reverts if block proof doesn't match valid block header lengths", async function () {
+      const { oracleAdapter, blocks } = await setup()
+      const blockHeaderProofContents = RLP.decode(blockRLP(blocks[0]))
+      const blockHeaderProofTooShortContents = blockHeaderProofContents.slice(0, 14)
+      const blockHeaderProofTooShort = RLP.encode(blockHeaderProofTooShortContents)
+
+      // Block proof RLP contains too few elements
+      await expect(oracleAdapter.proveAncestralHeaders(CHAIN_ID, [blockHeaderProofTooShort]))
+        .to.revertedWithCustomError(oracleAdapter, "InvalidBlockHeaderProofLength")
+        .withArgs(blockHeaderProofTooShortContents.length)
+
+      console.log(blockHeaderProofContents.length)
+      const blockHeaderProofTooLongContents = blockHeaderProofContents.concat([
+        oracleAdapter.address,
+        oracleAdapter.address,
+      ])
+      const blockHeaderProofTooLong = RLP.encode(blockHeaderProofTooLongContents)
+
+      // Block proof RLP contains too many elements
+      await expect(oracleAdapter.proveAncestralHeaders(CHAIN_ID, [blockHeaderProofTooLong]))
+        .to.revertedWithCustomError(oracleAdapter, "InvalidBlockHeaderProofLength")
+        .withArgs(blockHeaderProofTooLongContents.length)
     })
   })
 })
