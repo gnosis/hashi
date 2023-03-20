1c187c3a342b93edaa5e10191070af6779a29a37
Auryn Macmillan <hi@auryn.xyz>
2023-03-20 23:56:39 +1100

    feat: draft message executor

--- contracts/MessageExecutor.sol
+++ contracts/MessageExecutor.sol
@@ -1,4 +1,38 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.17;
 
-contract MessageExecutor {}
+import "./interfaces/IHashi.sol";
+import "./interfaces/IMessage.sol";
+
+contract MessageExecutor {
+    IHashi immutable hashi;
+    mapping(bytes32 => bool) public executed;
+
+    error UnequalArrayLengths(address emitter);
+    error HashMismatch(address emitter, uint256 id, bytes32 reportedHash, bytes32 calculatedHash);
+    error CallFailed(address emitter, uint256 id);
+
+    constructor(IHashi _hashi) {
+        hashi = _hashi;
+    }
+
+    function executeMessagesFromOracles(
+        Message[] memory messages,
+        uint256[] memory messageIds,
+        address[] memory senders,
+        IOracleAdapter[] memory oracleAdapters
+    ) public returns (bytes memory data) {
+        if (messages.length != senders.length || messages.length != messageIds.length)
+            revert UnequalArrayLengths(address(this));
+        for (uint i = 0; i < messages.length; i++) {
+            Message memory message = messages[i];
+            uint256 id = messageIds[i];
+            bytes32 reportedHash = hashi.getHash(message.toChainId, id, oracleAdapters);
+            bytes32 calculatedHash = keccak256(abi.encode(id, senders[i], msg.sender, message));
+            if (reportedHash != calculatedHash) revert HashMismatch(address(this), id, reportedHash, calculatedHash);
+            bool success;
+            (success, data) = address(message.to).call(message.data);
+            if (!success) revert CallFailed(address(this), id);
+        }
+    }
+}
