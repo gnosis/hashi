caff34a30f6e2128463456e6975202e01518dfb5
Auryn Macmillan <hi@auryn.xyz>
2023-03-21 00:00:06 +1100

    feat: check for previous execution

--- contracts/MessageExecutor.sol
+++ contracts/MessageExecutor.sol
@@ -6,9 +6,10 @@ import "./interfaces/IMessage.sol";
 
 contract MessageExecutor {
     IHashi immutable hashi;
-    mapping(bytes32 => bool) public executed;
+    mapping(uint256 => bool) public executed;
 
     error UnequalArrayLengths(address emitter);
+    error AlreadyExecuted(address emitter, uint256 id);
     error HashMismatch(address emitter, uint256 id, bytes32 reportedHash, bytes32 calculatedHash);
     error CallFailed(address emitter, uint256 id);
 
@@ -25,8 +26,9 @@ contract MessageExecutor {
         if (messages.length != senders.length || messages.length != messageIds.length)
             revert UnequalArrayLengths(address(this));
         for (uint i = 0; i < messages.length; i++) {
-            Message memory message = messages[i];
             uint256 id = messageIds[i];
+            if (executed[id]) revert AlreadyExecuted(address(this), id);
+            Message memory message = messages[i];
             bytes32 reportedHash = hashi.getHash(message.toChainId, id, oracleAdapters);
             bytes32 calculatedHash = keccak256(abi.encode(id, senders[i], msg.sender, message));
             if (reportedHash != calculatedHash) revert HashMismatch(address(this), id, reportedHash, calculatedHash);
