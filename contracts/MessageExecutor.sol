ff4915efc7738759738807593ba023cad6e750e4
Auryn Macmillan <hi@auryn.xyz>
2023-03-21 00:06:38 +1100

    chore: add event and fix return data

--- contracts/MessageExecutor.sol
+++ contracts/MessageExecutor.sol
@@ -8,6 +8,8 @@ contract MessageExecutor {
     IHashi immutable hashi;
     mapping(uint256 => bool) public executed;
 
+    event MessageIdExecuted(uint256 indexed fromChainId, bytes32 indexed messageId);
+
     error UnequalArrayLengths(address emitter);
     error AlreadyExecuted(address emitter, uint256 id);
     error HashMismatch(address emitter, uint256 id, bytes32 reportedHash, bytes32 calculatedHash);
@@ -22,19 +24,22 @@ contract MessageExecutor {
         uint256[] memory messageIds,
         address[] memory senders,
         IOracleAdapter[] memory oracleAdapters
-    ) public returns (bytes memory data) {
+    ) public returns (bytes[] memory returnDatas) {
         if (messages.length != senders.length || messages.length != messageIds.length)
             revert UnequalArrayLengths(address(this));
         for (uint i = 0; i < messages.length; i++) {
             uint256 id = messageIds[i];
             if (executed[id]) revert AlreadyExecuted(address(this), id);
+
             Message memory message = messages[i];
             bytes32 reportedHash = hashi.getHash(message.toChainId, id, oracleAdapters);
             bytes32 calculatedHash = keccak256(abi.encode(id, senders[i], msg.sender, message));
             if (reportedHash != calculatedHash) revert HashMismatch(address(this), id, reportedHash, calculatedHash);
-            bool success;
-            (success, data) = address(message.to).call(message.data);
+
+            (bool success, bytes memory returnData) = address(message.to).call(message.data);
             if (!success) revert CallFailed(address(this), id);
+            returnDatas[i] = returnData;
+            emit MessageIdExecuted(message.toChainId, bytes32(id));
         }
     }
 }
