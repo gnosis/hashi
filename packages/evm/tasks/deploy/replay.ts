f97fc4293d189c84423919427d2e53c4da1608b8
Auryn Macmillan <hi@auryn.xyz>
2023-06-08 01:26:19 +1000

    feat: catch deploy errors

--- packages/evm/tasks/deploy/replay.ts
+++ packages/evm/tasks/deploy/replay.ts
@@ -12,6 +12,10 @@ import type { HashiModule__factory } from "../../types/factories/contracts/zodia
 
 const ADDRESS_ONE = "0x0000000000000000000000000000000000000001"
 
+const logDeployError = function (error: any) {
+  console.log(`  \x1B[31m✘ Deployment failed:\x1B[0m ${error?.reason || error}`)
+}
+
 export const deploy = async ({ networks }: { networks: string[] }, hre: HardhatRuntimeEnvironment) => {
   const networkList = networks ? networks : Object.keys(hre.config.networks)
 
@@ -24,57 +28,85 @@ export const deploy = async ({ networks }: { networks: string[] }, hre: HardhatR
       const signer = hre.ethers.provider.getSigner(deployer.address)
 
       // deploy hashi
-      console.log(`\x1B[4mHashi\x1B[0m`)
-      const hashiFactory: Hashi__factory = <Hashi__factory>await hre.ethers.getContractFactory("Hashi")
-      await deployMastercopy(signer, hashiFactory, [], hre.ethers.constants.HashZero)
+      try {
+        console.log(`\x1B[4mHashi\x1B[0m`)
+        const hashiFactory: Hashi__factory = <Hashi__factory>await hre.ethers.getContractFactory("Hashi")
+        await deployMastercopy(signer, hashiFactory, [], hre.ethers.constants.HashZero)
+      } catch (error) {
+        logDeployError(error)
+      }
 
       // deploy yaho
-      console.log(`\x1B[4mYaho\x1B[0m`)
-      const yahoFactory: Yaho__factory = <Yaho__factory>await hre.ethers.getContractFactory("Yaho")
-      await deployMastercopy(signer, yahoFactory, [], hre.ethers.constants.HashZero)
+      try {
+        console.log(`\x1B[4mYaho\x1B[0m`)
+        const yahoFactory: Yaho__factory = <Yaho__factory>await hre.ethers.getContractFactory("Yaho")
+        await deployMastercopy(signer, yahoFactory, [], hre.ethers.constants.HashZero)
+      } catch (error) {
+        logDeployError(error)
+      }
 
       // deploy yaru
-      console.log(`\x1B[4mYaru\x1B[0m`)
-      const yaruFactory: Yaru__factory = <Yaru__factory>await hre.ethers.getContractFactory("Yaru")
-      await deployMastercopy(signer, yaruFactory, [ADDRESS_ONE, ADDRESS_ONE, 1], hre.ethers.constants.HashZero)
+      try {
+        console.log(`\x1B[4mYaru\x1B[0m`)
+        const yaruFactory: Yaru__factory = <Yaru__factory>await hre.ethers.getContractFactory("Yaru")
+        await deployMastercopy(signer, yaruFactory, [ADDRESS_ONE, ADDRESS_ONE, 1], hre.ethers.constants.HashZero)
+      } catch (error) {
+        logDeployError(error)
+      }
 
       // deploy shoyubashi
-      console.log(`\x1B[4mShoyuBashi\x1B[0m`)
-      const shoyuBashiFactory: ShoyuBashi__factory = <ShoyuBashi__factory>(
-        await hre.ethers.getContractFactory("ShoyuBashi")
-      )
-      await deployMastercopy(signer, shoyuBashiFactory, [ADDRESS_ONE, ADDRESS_ONE], hre.ethers.constants.HashZero)
+      try {
+        console.log(`\x1B[4mShoyuBashi\x1B[0m`)
+        const shoyuBashiFactory: ShoyuBashi__factory = <ShoyuBashi__factory>(
+          await hre.ethers.getContractFactory("ShoyuBashi")
+        )
+        await deployMastercopy(signer, shoyuBashiFactory, [ADDRESS_ONE, ADDRESS_ONE], hre.ethers.constants.HashZero)
+      } catch (error) {
+        logDeployError(error)
+      }
 
       // deploy girigiribashi
-      console.log(`\x1B[4mGiriGiriBashi\x1B[0m`)
-      const giriGiriBashiFactory: GiriGiriBashi__factory = <GiriGiriBashi__factory>(
-        await hre.ethers.getContractFactory("GiriGiriBashi")
-      )
-      await deployMastercopy(
-        signer,
-        giriGiriBashiFactory,
-        [ADDRESS_ONE, ADDRESS_ONE, ADDRESS_ONE],
-        hre.ethers.constants.HashZero,
-      )
+      try {
+        console.log(`\x1B[4mGiriGiriBashi\x1B[0m`)
+        const giriGiriBashiFactory: GiriGiriBashi__factory = <GiriGiriBashi__factory>(
+          await hre.ethers.getContractFactory("GiriGiriBashi")
+        )
+        await deployMastercopy(
+          signer,
+          giriGiriBashiFactory,
+          [ADDRESS_ONE, ADDRESS_ONE, ADDRESS_ONE],
+          hre.ethers.constants.HashZero,
+        )
+      } catch (error) {
+        logDeployError(error)
+      }
 
-      // deploy headstorage
-      console.log(`\x1B[4mHeader Storage\x1B[0m`)
-      const headerStorageFactory: HeaderStorage__factory = <HeaderStorage__factory>(
-        await hre.ethers.getContractFactory("HeaderStorage")
-      )
-      await deployMastercopy(signer, headerStorageFactory, [], hre.ethers.constants.HashZero)
+      // deploy header storage
+      try {
+        console.log(`\x1B[4mHeader Storage\x1B[0m`)
+        const headerStorageFactory: HeaderStorage__factory = <HeaderStorage__factory>(
+          await hre.ethers.getContractFactory("HeaderStorage")
+        )
+        await deployMastercopy(signer, headerStorageFactory, [], hre.ethers.constants.HashZero)
+      } catch (error) {
+        logDeployError(error)
+      }
 
       // deploy hashi module
-      console.log(`\x1B[4mHeader Storage\x1B[0m`)
-      const hashiModuleFactory: HashiModule__factory = <HashiModule__factory>(
-        await hre.ethers.getContractFactory("HashiModule")
-      )
-      await deployMastercopy(
-        signer,
-        hashiModuleFactory,
-        [ADDRESS_ONE, ADDRESS_ONE, ADDRESS_ONE, ADDRESS_ONE, ADDRESS_ONE, 1],
-        hre.ethers.constants.HashZero,
-      )
+      try {
+        console.log(`\x1B[4mHeader Storage\x1B[0m`)
+        const hashiModuleFactory: HashiModule__factory = <HashiModule__factory>(
+          await hre.ethers.getContractFactory("HashiModule")
+        )
+        await deployMastercopy(
+          signer,
+          hashiModuleFactory,
+          [ADDRESS_ONE, ADDRESS_ONE, ADDRESS_ONE, ADDRESS_ONE, ADDRESS_ONE, 1],
+          hre.ethers.constants.HashZero,
+        )
+      } catch (error) {
+        logDeployError(error)
+      }
     } catch (error: any) {
       console.log(`  \x1B[31m✘ Network skipped because:\x1B[0m            ${error?.reason || error}`)
     }
