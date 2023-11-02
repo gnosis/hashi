8963d36f729387e9dd2c78d83fe6ef1384c8e6e1
Alessandro Manfredi <alle.manfredi@gmail.com>
2023-11-02 10:18:43 +0100

    refactor(evm): adds onMessage within PingPong

--- packages/evm/contracts/test/PingPong.sol
+++ packages/evm/contracts/test/PingPong.sol
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.17;
 
-contract PingPong {
+import { IHashiReceiver } from "../interfaces/IHashiReceiver.sol";
+
+contract PingPong is IHashiReceiver {
     event Pong(string pong);
 
     uint256 public count;
 
-    function ping() public returns (string memory pong, bytes32, uint256, address) {
+    function ping() public returns (string memory pong) {
         count++;
         pong = "pong";
         emit Pong(pong);
+    }
+
+    function onMessage(bytes calldata, bytes32, uint256, address) external returns (bytes memory) {
+        return abi.encode(ping());
     }
 }
