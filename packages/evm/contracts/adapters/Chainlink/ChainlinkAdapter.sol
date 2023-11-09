cb0c127d7293b64290edacb70614b3eabce103ac
MikeKinetex <mike@kinetex.io>
2023-11-09 16:55:24 +0300

    feat: add Chainlink CCIP adapter

---
+++ packages/evm/contracts/adapters/Chainlink/ChainlinkAdapter.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.17;
+
+import { CCIPReceiver } from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import { Client } from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import { HeaderOracleAdapter } from "../HeaderOracleAdapter.sol";
+
+contract ChainlinkAdapter is HeaderOracleAdapter, CCIPReceiver {
+    string public constant PROVIDER = "chainlink";
+    uint64 public immutable CHAINLINK_REPORTER_CHAIN;
+
+    constructor(
+        uint256 reporterChain,
+        address reporterAddress,
+        address chainlinkRouter,
+        uint64 chainlinkReporterChain
+    ) HeaderOracleAdapter(reporterChain, reporterAddress) CCIPReceiver(chainlinkRouter) {
+        CHAINLINK_REPORTER_CHAIN = chainlinkReporterChain;
+    }
+
+    function _ccipReceive(Client.Any2EVMMessage memory message) internal virtual override {
+        // Validity of `msg.sender` is ensured by `CCIPReceiver` prior this internal function invocation
+        require(
+            message.sourceChainSelector == CHAINLINK_REPORTER_CHAIN &&
+                abi.decode(message.sender, (address)) == REPORTER_ADDRESS,
+            "LA: auth"
+        );
+        _receivePayload(message.data);
+    }
+}
---
+++ packages/evm/contracts/adapters/Chainlink/ChainlinkReporter.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.17;
+
+import { IRouterClient } from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import { Client } from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import { HeaderReporter } from "../HeaderReporter.sol";
+
+contract ChainlinkReporter is HeaderReporter {
+    string public constant PROVIDER = "chainlink";
+    address public immutable CHAINLINK_ROUTER;
+    uint64 public immutable CHAINLINK_ADAPTER_CHAIN;
+
+    constructor(
+        address headerStorage,
+        uint64 adapterChain,
+        address adapterAddress,
+        address chainlinkRouter,
+        uint64 chainlinkAdapterChain
+    ) HeaderReporter(headerStorage, adapterChain, adapterAddress) {
+        require(chainlinkRouter != address(0), "LR: invalid ctor call");
+        CHAINLINK_ROUTER = chainlinkRouter;
+        CHAINLINK_ADAPTER_CHAIN = chainlinkAdapterChain;
+    }
+
+    function _sendPayload(bytes memory payload) internal virtual override {
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(ADAPTER_ADDRESS),
+            data: payload,
+            tokenAmounts: new Client.EVMTokenAmount[](0), // Empty array - no tokens are transferred
+            extraArgs: Client._argsToBytes(Client.EVMExtraArgsV1({ gasLimit: 200_000, strict: false })),
+            feeToken: address(0) // Pay fees with native
+        });
+        IRouterClient(CHAINLINK_ROUTER).ccipSend{ value: msg.value }(CHAINLINK_ADAPTER_CHAIN, message);
+    }
+}
