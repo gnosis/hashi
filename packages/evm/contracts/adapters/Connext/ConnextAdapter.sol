c1ed8a7a46b471ad1c83c1ec5c3b25042c969175
MikeKinetex <mike@kinetex.io>
2023-11-22 23:38:44 +0400

    feat: update Connext adapter

--- packages/evm/contracts/adapters/Connext/ConnextAdapter.sol
+++ packages/evm/contracts/adapters/Connext/ConnextAdapter.sol
@@ -1,75 +1,38 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.17;
 
-import { IConnext } from "@connext/interfaces/core/IConnext.sol";
 import { IXReceiver } from "@connext/interfaces/core/IXReceiver.sol";
-import { OracleAdapter } from "../OracleAdapter.sol";
-import { BlockHashOracleAdapter } from "../BlockHashOracleAdapter.sol";
+import { HeaderOracleAdapter } from "../HeaderOracleAdapter.sol";
 
-contract ConnextAdapter is OracleAdapter, BlockHashOracleAdapter, IXReceiver {
-    bytes32 public headerReporter;
-
-    error InvalidInputs();
-    error InvalidSource(address _originSender, uint32 _origin);
-
-    mapping(uint32 => uint16) public domainToChainId;
-    mapping(uint32 => address) public domainToSource;
-    mapping(uint32 => address) public domainToConnext;
+contract ConnextAdapter is HeaderOracleAdapter, IXReceiver {
+    string public constant PROVIDER = "connext";
+    address public immutable CONNEXT;
+    uint32 public immutable CONNEXT_REPORTER_CHAIN;
 
     constructor(
-        address[] memory _connexts,
-        uint32[] memory _originDomains,
-        uint16[] memory _originChainIds,
-        address[] memory _sources,
-        bytes32 _headerReporter
-    ) {
-        headerReporter = _headerReporter;
-        if (
-            _originDomains.length != _connexts.length ||
-            _originDomains.length != _originChainIds.length ||
-            _originDomains.length != _sources.length
-        ) {
-            revert InvalidInputs();
-        }
-        for (uint256 i = 0; i < _originDomains.length; i++) {
-            domainToChainId[_originDomains[i]] = _originChainIds[i];
-            domainToSource[_originDomains[i]] = _sources[i];
-            domainToConnext[_originDomains[i]] = address(_connexts[i]);
-        }
+        uint256 reporterChain,
+        address reporterAddress,
+        address connext,
+        uint32 connextReporterChain
+    ) HeaderOracleAdapter(reporterChain, reporterAddress) {
+        require(connext != address(0), "CA: invalid ctor call");
+        CONNEXT = connext;
+        CONNEXT_REPORTER_CHAIN = connextReporterChain;
     }
 
-    /** @notice A modifier for authenticated calls.
-     * This is an important security consideration. If the target contract
-     * function should be authenticated, it must check three things:
-     *    1) The originating call comes from the expected origin domain.
-     *    2) The originating call comes from the expected source contract.
-     *    3) The call to this contract comes from Connext.
-     */
-    modifier onlySource(address _originSender, uint32 _origin) {
-        if (domainToSource[_origin] != _originSender || msg.sender != domainToConnext[_origin]) {
-            revert InvalidSource(_originSender, _origin);
-        }
-        _;
-    }
-
-    /// @dev Stores the block header for a given block.
-    /// @param _transferId Unique Identifier for the transfer.
-    /// @param _amount Amount of tokens transferred (0 in this case).
-    /// @param _asset Asset transferred (irrelevant).
-    /// @param _originSender Address which originated the call on the origin chain.
-    /// @param _origin Domain for the origin chain.
-    /// @param _callData decodable calldata passed through the Connext bridge.
-    /// @notice Only callable by `connext` with a message passed from `headerReporter.
     function xReceive(
-        bytes32 _transferId, // solhint-disable-line
-        uint256 _amount, // solhint-disable-line
-        address _asset, // solhint-disable-line
-        address _originSender,
-        uint32 _origin,
-        bytes memory _callData
-    ) external onlySource(_originSender, _origin) returns (bytes memory) {
-        // Unpack the _callData
-        (uint256 blockNumber, bytes32 newBlockHeader) = abi.decode(_callData, (uint256, bytes32));
-        _storeHash(uint256(domainToChainId[_origin]), blockNumber, newBlockHeader);
+        bytes32 /* transferId_ */,
+        uint256 /* amount_ */,
+        address /* asset_ */,
+        address originSender,
+        uint32 origin,
+        bytes memory callData
+    ) external returns (bytes memory) {
+        require(
+            msg.sender == CONNEXT && origin == CONNEXT_REPORTER_CHAIN && originSender == REPORTER_ADDRESS,
+            "CA: auth"
+        );
+        _receivePayload(callData);
+        return "";
     }
 }
---
+++ packages/evm/contracts/adapters/Connext/ConnextReporter.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.17;
+
+import { IConnext } from "@connext/interfaces/core/IConnext.sol";
+import { HeaderReporter } from "../HeaderReporter.sol";
+
+contract ConnextReporter is HeaderReporter {
+    string public constant PROVIDER = "connext";
+    address public immutable CONNEXT;
+    uint32 public immutable CONNEXT_ADAPTER_CHAIN;
+
+    event ConnextTransfer(bytes32 transferId);
+
+    constructor(
+        address headerStorage,
+        uint256 adapterChain,
+        address adapterAddress,
+        address connext,
+        uint32 connextAdapterChain
+    ) HeaderReporter(headerStorage, adapterChain, adapterAddress) {
+        require(connext != address(0), "CR: invalid ctor call");
+        CONNEXT = connext;
+        CONNEXT_ADAPTER_CHAIN = connextAdapterChain;
+    }
+
+    function _sendPayload(bytes memory payload) internal override {
+        bytes32 transferId = IConnext(CONNEXT).xcall{ value: msg.value }(
+            CONNEXT_ADAPTER_CHAIN, // _destination: Domain ID of the destination chain
+            ADAPTER_ADDRESS, // _to: address of the target contract
+            address(0), // _asset: use address zero for 0-value transfers
+            msg.sender, // _delegate: address that can revert or forceLocal on destination
+            0, // _amount: 0 because no funds are being transferred
+            0, // _slippage: can be anything between 0-10000 because no funds are being transferred
+            payload // _callData: the encoded calldata to send
+        );
+        emit ConnextTransfer(transferId);
+    }
+}
