e7c61b0d2ca253b87fb99717a84aa5f69d528810
MikeKinetex <mike@kinetex.io>
2023-11-24 03:14:47 +0400

    refactor: rename CCIP adapter

--- packages/evm/contracts/adapters/Chainlink/CCIPAdapter.sol
+++ packages/evm/contracts/adapters/Chainlink/CCIPAdapter.sol
@@ -5,27 +5,27 @@ import { CCIPReceiver } from "@chainlink/contracts-ccip/src/v0.8/ccip/applicatio
 import { Client } from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
 import { HeaderOracleAdapter } from "../HeaderOracleAdapter.sol";
 
-contract ChainlinkAdapter is HeaderOracleAdapter, CCIPReceiver {
-    string public constant PROVIDER = "chainlink";
-    uint64 public immutable CHAINLINK_REPORTER_CHAIN;
+contract CCIPAdapter is HeaderOracleAdapter, CCIPReceiver {
+    string public constant PROVIDER = "ccip";
+    uint64 public immutable CCIP_REPORTER_CHAIN;
 
-    error UnauthorizedChainlinkReceive();
+    error UnauthorizedCCIPReceive();
 
     constructor(
         uint256 reporterChain,
         address reporterAddress,
-        address chainlinkRouter,
-        uint64 chainlinkReporterChain
-    ) HeaderOracleAdapter(reporterChain, reporterAddress) CCIPReceiver(chainlinkRouter) {
-        CHAINLINK_REPORTER_CHAIN = chainlinkReporterChain;
+        address ccipRouter,
+        uint64 ccipReporterChain
+    ) HeaderOracleAdapter(reporterChain, reporterAddress) CCIPReceiver(ccipRouter) {
+        CCIP_REPORTER_CHAIN = ccipReporterChain;
     }
 
     function _ccipReceive(Client.Any2EVMMessage memory message) internal override {
         // Validity of `msg.sender` is ensured by `CCIPReceiver` prior this internal function invocation
         if (
-            message.sourceChainSelector != CHAINLINK_REPORTER_CHAIN ||
+            message.sourceChainSelector != CCIP_REPORTER_CHAIN ||
             abi.decode(message.sender, (address)) != REPORTER_ADDRESS
-        ) revert UnauthorizedChainlinkReceive();
+        ) revert UnauthorizedCCIPReceive();
         _receivePayload(message.data);
     }
 }
--- packages/evm/contracts/adapters/Chainlink/CCIPReporter.sol
+++ packages/evm/contracts/adapters/Chainlink/CCIPReporter.sol
@@ -4,17 +4,17 @@ pragma solidity ^0.8.17;
 import { IRouterClient } from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import { Client } from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
 
-abstract contract ChainlinkReporter {
-    string public constant PROVIDER = "chainlink";
-    IRouterClient public immutable CHAINLINK_ROUTER;
-    uint64 public immutable CHAINLINK_ADAPTER_CHAIN;
+abstract contract CCIPReporter {
+    string public constant PROVIDER = "ccip";
+    IRouterClient public immutable CCIP_ROUTER;
+    uint64 public immutable CCIP_ADAPTER_CHAIN;
 
-    constructor(address chainlinkRouter, uint64 chainlinkAdapterChain) {
-        CHAINLINK_ROUTER = IRouterClient(chainlinkRouter);
-        CHAINLINK_ADAPTER_CHAIN = chainlinkAdapterChain;
+    constructor(address ccipRouter, uint64 ccipAdapterChain) {
+        CCIP_ROUTER = IRouterClient(ccipRouter);
+        CCIP_ADAPTER_CHAIN = ccipAdapterChain;
     }
 
-    function _chainlinkSend(bytes memory payload, address adapter) internal {
+    function _ccipSend(bytes memory payload, address adapter) internal {
         Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
             receiver: abi.encode(adapter),
             data: payload,
@@ -22,6 +22,6 @@ abstract contract ChainlinkReporter {
             extraArgs: Client._argsToBytes(Client.EVMExtraArgsV1({ gasLimit: 200_000, strict: false })),
             feeToken: address(0) // Pay fees with native
         });
-        CHAINLINK_ROUTER.ccipSend{ value: msg.value }(CHAINLINK_ADAPTER_CHAIN, message);
+        CCIP_ROUTER.ccipSend{ value: msg.value }(CCIP_ADAPTER_CHAIN, message);
     }
 }
