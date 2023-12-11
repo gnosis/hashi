5ba9ba347a791a2e8b5248ff9ac9b877fcbad0f9
Alain Olivier <oliviera9@gmail.com>
2023-12-11 15:44:47 +0100

    refactor(evm): remove unnecessary import in pNetwork adapter

--- packages/evm/contracts/adapters/pnetwork/PNetworkAdapter.sol
+++ packages/evm/contracts/adapters/pnetwork/PNetworkAdapter.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC777/presets/ERC777PresetFixedSupply.sol";
-
 import { HeaderOracleAdapter } from "../HeaderOracleAdapter.sol";
 import { PNetworkBase } from "./PNetworkBase.sol";
 
