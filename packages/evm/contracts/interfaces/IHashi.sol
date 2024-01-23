11cddd9d1aefe3fc176626ec7f84ad56f0de2556
Alessandro Manfredi <alle.manfredi@gmail.com>
2024-01-23 17:58:49 +0100

    refactor(evm): adds fx descriptions within IHashi

--- packages/evm/contracts/interfaces/IHashi.sol
+++ packages/evm/contracts/interfaces/IHashi.sol
@@ -3,17 +3,22 @@ pragma solidity ^0.8.17;
 
 import { IOracleAdapter } from "./IOracleAdapter.sol";
 
+/**
+ * @title IHashi
+ */
 interface IHashi {
     error NoOracleAdaptersGiven();
     error OracleDidNotReport(IOracleAdapter oracleAdapter);
     error OraclesDisagree(IOracleAdapter oracleOne, IOracleAdapter oracleTwo);
 
-    /// @dev Checks whether threshold is reached for a message given a set of oracleAdapters
-    /// @param domain ID of the domain to query.
-    /// @param id ID for which to return hash.
-    /// @param threshold to use.
-    /// @param oracleAdapters Array of address for the oracle adapters to query.
-    /// @return a boolean indicating if a threshold for a given message has been reached.
+    /**
+     * @dev Checks whether the threshold is reached for a message given a set of oracleAdapters.
+     * @param domain - ID of the domain to query.
+     * @param id - ID for which to return hash.
+     * @param threshold - Threshold to use.
+     * @param oracleAdapters - Array of addresses for the oracle adapters to query.
+     * @return result A boolean indicating if a threshold for a given message has been reached.
+     */
     function checkHashWithThresholdFromOracles(
         uint256 domain,
         uint256 id,
@@ -21,34 +26,40 @@ interface IHashi {
         IOracleAdapter[] calldata oracleAdapters
     ) external view returns (bool);
 
-    /// @dev Returns the hash reported by a given oracle for a given ID.
-    /// @param domain Id of the domain to query.
-    /// @param id ID for which to return a hash.
-    /// @param oracleAdapter Address of the oracle adapter to query.
-    /// @return hash Hash reported by the given oracle adapter for the given ID number.
+    /**
+     * @dev Returns the hash reported by a given oracle for a given ID.
+     * @param domain - ID of the domain to query.
+     * @param id - ID for which to return a hash.
+     * @param oracleAdapter - Address of the oracle adapter to query.
+     * @return hash reported by the given oracle adapter for the given ID.
+     */
     function getHashFromOracle(
         uint256 domain,
         uint256 id,
         IOracleAdapter oracleAdapter
     ) external view returns (bytes32);
 
-    /// @dev Returns the hash for a given ID reported by a given set of oracles.
-    /// @param domain ID of the domain to query.
-    /// @param id ID for which to return hashs.
-    /// @param oracleAdapters Array of address for the oracle adapters to query.
-    /// @return hashes Array of hash reported by the given oracle adapters for the given ID.
+    /**
+     * @dev Returns the hashes for a given ID reported by a given set of oracles.
+     * @param domain - The ID of the domain to query.
+     * @param id - The ID for which to return hashes.
+     * @param oracleAdapters - An array of addresses for the oracle adapters to query.
+     * @return hashes An array of hashes reported by the given oracle adapters for the specified ID.
+     */
     function getHashesFromOracles(
         uint256 domain,
         uint256 id,
         IOracleAdapter[] calldata oracleAdapters
     ) external view returns (bytes32[] memory);
 
-    /// @dev Returns the hash unanimously agreed upon by a given set of oracles.
-    /// @param domain ID of the domain to query.
-    /// @param id ID for which to return hash.
-    /// @param oracleAdapters Array of address for the oracle adapters to query.
-    /// @return hash Hash agreed on by the given set of oracle adapters.
-    /// @notice MUST revert if oracles disagree on the hash or if an oracle does not report.
+    /**
+     * @dev Returns the hash unanimously agreed upon by a given set of oracles.
+     * @param domain - The ID of the domain to query.
+     * @param id - The ID for which to return a hash.
+     * @param oracleAdapters - An array of addresses for the oracle adapters to query.
+     * @return hash agreed on by the given set of oracle adapters.
+     * @notice MUST revert if oracles disagree on the hash or if an oracle does not report.
+     */
     function getHash(
         uint256 domain,
         uint256 id,
