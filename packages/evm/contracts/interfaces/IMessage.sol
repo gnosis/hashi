652e508c312baaf33345a3ccabace820e869aab0
Alessandro Manfredi <alle.manfredi@gmail.com>
2024-01-23 15:58:50 +0100

    feat(evm): changes Message struct

--- packages/evm/contracts/interfaces/IMessage.sol
+++ packages/evm/contracts/interfaces/IMessage.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.17;
 
+import { IOracleAdapter } from "./IOracleAdapter.sol";
+
 struct Message {
-    address to;
+    bytes32 salt;
     uint256 toChainId;
+    uint256 threshold;
+    address sender;
+    address receiver;
     bytes data;
+    address[] reporters;
+    IOracleAdapter[] adapters;
 }
