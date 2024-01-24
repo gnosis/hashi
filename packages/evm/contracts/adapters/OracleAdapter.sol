3fa6771057ebaf98141f473d263e56604d6ad9eb
Alessandro Manfredi <alle.manfredi@gmail.com>
2024-01-24 07:43:20 +0100

    refactor(evm): changes OracleAdapter comments

--- packages/evm/contracts/adapters/OracleAdapter.sol
+++ packages/evm/contracts/adapters/OracleAdapter.sol
@@ -6,19 +6,11 @@ import { IOracleAdapter } from "../interfaces/IOracleAdapter.sol";
 abstract contract OracleAdapter is IOracleAdapter {
     mapping(uint256 => mapping(uint256 => bytes32)) public hashes;
 
-    /// @dev Returns the hash for a given domain and ID, as reported by the oracle.
-    /// @param domain Identifier for the domain to query.
-    /// @param id Identifier for the ID to query.
-    /// @return hash Bytes32 hash reported by the oracle for the given ID on the given domain.
-    /// @notice MUST return bytes32(0) if the oracle has not yet reported a hash for the given ID.
+    /// @inheritdoc IOracleAdapter
     function getHashFromOracle(uint256 domain, uint256 id) external view returns (bytes32 hash) {
         hash = hashes[domain][id];
     }
 
-    /// @dev Stores a hash for a given domain and ID.
-    /// @param domain Identifier for the domain.
-    /// @param id Identifier for the ID of the hash.
-    /// @param hash Bytes32 hash value to store.
     function _storeHash(uint256 domain, uint256 id, bytes32 hash) internal {
         bytes32 currentHash = hashes[domain][id];
         if (currentHash != hash) {
--- packages/evm/contracts/interfaces/IOracleAdapter.sol
+++ packages/evm/contracts/interfaces/IOracleAdapter.sol
@@ -8,10 +8,13 @@ interface IOracleAdapter {
     error InvalidBlockHeaderRLP();
     error ConflictingBlockHeader(uint256 blockNumber, bytes32 reportedBlockHash, bytes32 storedBlockHash);
 
-    /// @dev Returns the hash for a given ID, as reported by the oracle.
-    /// @param domain Identifier for the domain to query.
-    /// @param id Identifier for the ID to query.
-    /// @return hash Bytes32 hash reported by the oracle for the given ID on the given domain.
-    /// @notice MUST return bytes32(0) if the oracle has not yet reported a hash for the given ID.
-    function getHashFromOracle(uint256 domain, uint256 id) external view returns (bytes32 hash);
+    /**
+     * @dev Returns the hash for a given domain and ID, as reported by the oracle
+     *
+     * @param domain - The identifier for the domain to query.
+     * @param id - The identifier for which the hash is being queried.
+     * @return hash The bytes32 hash reported by the oracle for the given ID on the specified domain.
+     * @notice This function will return a default value of bytes32(0) if the oracle has not yet reported a hash for the given ID.
+     */
+    function getHashFromOracle(uint256 domain, uint256 id) external view returns (bytes32);
 }
