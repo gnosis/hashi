dd8d57a705f51d9212a307e6fa8a36e60af45fb3
Alessandro Manfredi <alle.manfredi@gmail.com>
2024-01-24 16:41:40 +0100

    feat(evm): adds ConnextReporter and refactors ConnextAdapter

--- packages/evm/contracts/adapters/Connext/ConnextReporter.sol
+++ packages/evm/contracts/adapters/Connext/ConnextReporter.sol
@@ -1,23 +1,41 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.17;
 
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { IConnext } from "@connext/interfaces/core/IConnext.sol";
+import { Reporter } from "../Reporter.sol";
 
-abstract contract ConnextReporter {
+contract ConnextReporter is Reporter, Ownable {
     string public constant PROVIDER = "connext";
     IConnext public immutable CONNEXT;
-    uint32 public immutable CONNEXT_ADAPTER_CHAIN;
 
+    mapping(uint256 => uint32) public domainIds;
     event ConnextTransfer(bytes32 transferId);
 
-    constructor(address connext, uint32 connextAdapterChain) {
+    error DomainIdNotAvailable();
+
+    event DomainIdSet(uint256 indexed chainId, uint32 indexed domainId);
+
+    constructor(address headerStorage, address yaho, address connext) Reporter(headerStorage, yaho) {
         CONNEXT = IConnext(connext);
-        CONNEXT_ADAPTER_CHAIN = connextAdapterChain;
     }
 
-    function _connextSend(bytes memory payload, address adapter) internal {
+    function setDomainIdByChainId(uint256 chainId, uint32 domainId) external onlyOwner {
+        domainIds[chainId] = domainId;
+        emit DomainIdSet(chainId, domainId);
+    }
+
+    function _dispatch(
+        uint256 toChainId,
+        address adapter,
+        uint256[] memory ids,
+        bytes32[] memory hashes
+    ) internal override returns (bytes32) {
+        uint32 domainId = domainIds[toChainId];
+        if (domainId == 0) revert DomainIdNotAvailable();
+        bytes memory payload = abi.encode(ids, hashes);
         bytes32 transferId = CONNEXT.xcall{ value: msg.value }(
-            CONNEXT_ADAPTER_CHAIN, // _destination: Domain ID of the destination chain
+            domainId, // _destination: Domain ID of the destination chain
             adapter, // _to: address of the target contract
             address(0), // _asset: use address zero for 0-value transfers
             msg.sender, // _delegate: address that can revert or forceLocal on destination
@@ -26,5 +44,7 @@ abstract contract ConnextReporter {
             payload // _callData: the encoded calldata to send
         );
         emit ConnextTransfer(transferId);
+
+        return bytes32(0);
     }
 }
