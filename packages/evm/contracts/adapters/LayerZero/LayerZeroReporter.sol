43bef50aa087b9bbcdaf00171ccb5969b14324ca
Alessandro Manfredi <alle.manfredi@gmail.com>
2024-01-24 17:42:42 +0100

    feat(evm): adds LayerZeroReporter and refactors LayerZeroAdapter

--- packages/evm/contracts/adapters/LayerZero/LayerZeroReporter.sol
+++ packages/evm/contracts/adapters/LayerZero/LayerZeroReporter.sol
@@ -1,28 +1,48 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.17;
 
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ILayerZeroEndpoint } from "./interfaces/ILayerZeroEndpoint.sol";
+import { Reporter } from "../Reporter.sol";
 
-abstract contract LayerZeroReporter {
+contract LayerZeroReporter is Reporter, Ownable {
     string public constant PROVIDER = "layer-zero";
     ILayerZeroEndpoint public immutable LAYER_ZERO_ENDPOINT;
-    uint16 public immutable LAYER_ZERO_ADAPTER_CHAIN;
 
-    constructor(address lzEndpoint, uint16 lzAdapterChain) {
+    mapping(uint256 => uint16) public endpointIds;
+
+    error EndpointIdNotAvailable();
+
+    event EndpointIdSet(uint256 indexed chainId, uint16 indexed endpointId);
+
+    constructor(address headerStorage, address yaho, address lzEndpoint) Reporter(headerStorage, yaho) {
         LAYER_ZERO_ENDPOINT = ILayerZeroEndpoint(lzEndpoint);
-        LAYER_ZERO_ADAPTER_CHAIN = lzAdapterChain;
     }
 
-    function _lzSend(bytes memory payload, address adapter) internal {
+    function setEndpointIdByChainId(uint256 chainId, uint16 endpointId) external onlyOwner {
+        endpointIds[chainId] = endpointId;
+        emit EndpointIdSet(chainId, endpointId);
+    }
+
+    function _dispatch(
+        uint256 toChainId,
+        address adapter,
+        uint256[] memory ids,
+        bytes32[] memory hashes
+    ) internal override returns (bytes32) {
+        uint16 endpointId = endpointIds[toChainId];
+        if (endpointId == 0) revert EndpointIdNotAvailable();
+        bytes memory payload = abi.encode(ids, hashes);
         bytes memory path = abi.encodePacked(adapter, address(this));
         // solhint-disable-next-line check-send-result
         LAYER_ZERO_ENDPOINT.send{ value: msg.value }(
-            LAYER_ZERO_ADAPTER_CHAIN, // _dstChainId: destination LayerZero chainId
-            path, // _destination: send to this address on the destination
-            payload, // _payload: bytes payload
+            endpointId,
+            path,
+            payload,
             payable(msg.sender), // _refundAddress: refund address
             address(0), // _zroPaymentAddress: future parameter
             bytes("") // _adapterParams: adapterParams (see "Advanced Features")
         );
+        return bytes32(0);
     }
 }
