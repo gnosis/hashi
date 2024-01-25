4cdd1849eceb21f422f48120a7ec774a103d939e
Alessandro Manfredi <alle.manfredi@gmail.com>
2024-01-25 07:48:33 +0100

    refactor(evm): refactors Sygma Adapter and adds the Reporter

--- packages/evm/contracts/adapters/Sygma/SygmaReporter.sol
+++ packages/evm/contracts/adapters/Sygma/SygmaReporter.sol
@@ -1,29 +1,46 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.17;
 
-import "./interfaces/ISygmaAdapter.sol";
-import "./interfaces/IBridge.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { Reporter } from "../Reporter.sol";
+import { ISygmaAdapter } from "./interfaces/ISygmaAdapter.sol";
+import { IBridge } from "./interfaces/IBridge.sol";
 
-contract SygmaReporter {
-    address public immutable _bridge;
-    bytes32 public immutable _resourceID;
-    uint8 public immutable _defaultDestinationDomainID;
-    address public immutable _defaultSygmaAdapter;
+contract SygmaReporter is Reporter, Ownable {
+    string public constant PROVIDER = "sygma";
 
-    constructor(address bridge, bytes32 resourceID, uint8 defaultDestinationDomainID, address defaultSygmaAdapter) {
-        _bridge = bridge;
-        _resourceID = resourceID;
-        _defaultDestinationDomainID = defaultDestinationDomainID;
-        _defaultSygmaAdapter = defaultSygmaAdapter;
+    IBridge public immutable BRIDGE;
+
+    mapping(uint256 => uint8) public domainIds;
+    mapping(uint256 => bytes32) public resourceIds;
+
+    error DomainIdNotAvailable();
+    error ResourceIdNotAvailable();
+
+    event DomainIdSet(uint256 indexed chainId, uint8 indexed domainId);
+    event ResourceIdSet(uint256 indexed chainId, bytes32 indexed resourceId);
+
+    constructor(address headerStorage, address yaho, address bridge) Reporter(headerStorage, yaho) {
+        BRIDGE = IBridge(bridge);
     }
 
-    function _reportData(
-        uint256[] memory messageIds,
-        bytes32[] memory hashes,
-        address sygmaAdapter,
-        uint8 destinationDomainID,
-        bytes memory feeData
-    ) internal returns (uint64 depositNonce, bytes memory handlerResponse) {
+    function setDomainIdAndResourceIdByChainId(uint256 chainId, uint8 domainId, bytes32 resourceId) external onlyOwner {
+        domainIds[chainId] = domainId;
+        resourceIds[chainId] = resourceId;
+        emit DomainIdSet(chainId, domainId);
+        emit ResourceIdSet(chainId, resourceId);
+    }
+
+    function _dispatch(
+        uint256 toChainId,
+        address adapter,
+        uint256[] memory ids,
+        bytes32[] memory hashes
+    ) internal override returns (bytes32) {
+        uint8 domainId = domainIds[toChainId];
+        if (domainId == 0) revert DomainIdNotAvailable();
+        bytes32 resourceId = resourceIds[toChainId];
+        if (resourceId == bytes32(0)) revert ResourceIdNotAvailable();
         bytes memory depositData = abi.encodePacked(
             // uint256 maxFee
             uint256(950000),
@@ -34,15 +51,21 @@ contract SygmaReporter {
             // uint8 len(executeContractAddress)
             uint8(20),
             // bytes executeContractAddress
-            sygmaAdapter,
+            adapter,
             // uint8 len(executionDataDepositor)
             uint8(20),
             // bytes executionDataDepositor
             address(this),
             // bytes executionDataDepositor + executionData
-            prepareDepositData(messageIds, hashes)
+            prepareDepositData(ids, hashes)
         );
-        return IBridge(_bridge).deposit{ value: msg.value }(destinationDomainID, _resourceID, depositData, feeData);
+        (uint64 nonce, bytes memory handlerResponse) = BRIDGE.deposit{ value: msg.value }(
+            domainId,
+            resourceId,
+            depositData,
+            "" // feeData
+        );
+        return bytes32(keccak256(abi.encode(nonce, handlerResponse)));
     }
 
     function slice(bytes calldata input, uint256 position) public pure returns (bytes memory) {
