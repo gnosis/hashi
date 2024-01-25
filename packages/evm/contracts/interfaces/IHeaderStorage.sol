3fd0802144bd08b8773b62e84554e275bbf70e6b
Alessandro Manfredi <alle.manfredi@gmail.com>
2024-01-25 17:18:44 +0100

    refactor(evm): normalizes HeaderStorage

--- packages/evm/contracts/interfaces/IHeaderStorage.sol
+++ packages/evm/contracts/interfaces/IHeaderStorage.sol
@@ -2,11 +2,23 @@
 pragma solidity ^0.8.17;
 
 interface IHeaderStorage {
-    event HeaderStored(uint256 indexed blockNumber, bytes32 indexed blockHeader);
-
     error HeaderOutOfRange(address emitter, uint256 blockNumber);
 
-    function storeBlockHeader(uint256 blockNumber) external returns (bytes32 blockHeader);
+    event HeaderStored(uint256 indexed blockNumber, bytes32 indexed blockHeader);
 
+    /**
+     * @dev Stores and returns the header for the given block.
+     * @param blockNumber - Block number.
+     * @return blockHeader - Block header stored.
+     * @notice Reverts if the given block header was not previously stored and is now out of range.
+     */
+    function storeBlockHeader(uint256 blockNumber) external returns (bytes32);
+
+    /**
+     * @dev Stores and returns the header for an array of given blocks.
+     * @param blockNumbers - Array of block numbers.
+     * @return blockHeaders - Array of block headers stored.
+     * @notice Reverts if the given block header was not previously stored and is now out of range.
+     */
     function storeBlockHeaders(uint256[] memory blockNumbers) external returns (bytes32[] memory);
 }
