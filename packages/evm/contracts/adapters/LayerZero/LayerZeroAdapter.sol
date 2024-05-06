f2c56bec396ca0639f8a68846f8d7a0e46dda7c0
Alessandro Manfredi <alle.manfredi@gmail.com>
2024-05-06 15:56:21 +0200

    feat(contracts): updates l0 adapter and reporter for v2

--- packages/evm/contracts/adapters/LayerZero/LayerZeroAdapter.sol
+++ packages/evm/contracts/adapters/LayerZero/LayerZeroAdapter.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.20;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { ILayerZeroReceiver } from "./interfaces/ILayerZeroReceiver.sol";
 import { BlockHashAdapter } from "../BlockHashAdapter.sol";
+import { ILayerZeroEndpointV2, Origin } from "./interfaces/ILayerZeroEndpointV2.sol";
 
-contract LayerZeroAdapter is BlockHashAdapter, Ownable, ILayerZeroReceiver {
+contract LayerZeroAdapter is BlockHashAdapter, Ownable {
     string public constant PROVIDER = "layer-zero";
     address public immutable LAYER_ZERO_ENDPOINT;
 
-    mapping(uint32 => bytes32) public enabledReportersPaths;
+    mapping(uint32 => bytes32) public enabledReporters;
     mapping(uint32 => uint256) public chainIds;
 
     error UnauthorizedLayerZeroReceive();
@@ -20,17 +20,22 @@ contract LayerZeroAdapter is BlockHashAdapter, Ownable, ILayerZeroReceiver {
         LAYER_ZERO_ENDPOINT = lzEndpoint;
     }
 
-    function lzReceive(uint16 srcEndpointId, bytes memory srcPath, uint64 /* nonce */, bytes memory payload) external {
-        if (msg.sender != LAYER_ZERO_ENDPOINT || enabledReportersPaths[srcEndpointId] != keccak256(srcPath))
-            revert UnauthorizedLayerZeroReceive();
-        uint256 sourceChainId = chainIds[srcEndpointId];
-        (uint256[] memory ids, bytes32[] memory hashes) = abi.decode(payload, (uint256[], bytes32[]));
-        _storeHashes(sourceChainId, ids, hashes);
-    }
-
     function setReporterByChain(uint256 chainId, uint16 endpointId, address reporter) external onlyOwner {
-        enabledReportersPaths[endpointId] = keccak256(abi.encodePacked(reporter, address(this)));
+        enabledReporters[endpointId] = bytes32(abi.encodePacked(reporter));
         chainIds[endpointId] = chainId;
         emit ReporterSet(chainId, endpointId, reporter);
+    }
+
+    function lzReceive(
+        Origin calldata _origin,
+        bytes32 /*_guid*/,
+        bytes calldata _message,
+        address /*_executor*/,
+        bytes calldata /*_extraData*/
+    ) external payable {
+        if (msg.sender != LAYER_ZERO_ENDPOINT || enabledReporters[_origin.srcEid] != _origin.sender)
+            revert UnauthorizedLayerZeroReceive();
+        (uint256[] memory ids, bytes32[] memory hashes) = abi.decode(_message, (uint256[], bytes32[]));
+        _storeHashes(chainIds[_origin.srcEid], ids, hashes);
     }
 }
---
+++ packages/evm/contracts/adapters/LayerZero/interfaces/ILayerZeroEndpointV2.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0;
+
+struct MessagingParams {
+    uint32 dstEid;
+    bytes32 receiver;
+    bytes message;
+    bytes options;
+    bool payInLzToken;
+}
+
+struct MessagingReceipt {
+    bytes32 guid;
+    uint64 nonce;
+    MessagingFee fee;
+}
+
+struct MessagingFee {
+    uint256 nativeFee;
+    uint256 lzTokenFee;
+}
+
+struct Origin {
+    uint32 srcEid;
+    bytes32 sender;
+    uint64 nonce;
+}
+
+interface ILayerZeroEndpointV2 {
+    function send(
+        MessagingParams calldata _params,
+        address _refundAddress
+    ) external payable returns (MessagingReceipt memory);
+}
