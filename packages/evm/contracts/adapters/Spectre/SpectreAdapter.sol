3900283c4a46a9f3c841aeb7840c4beb41d91f73
nmlinaric <nikola.mlinaric93@gmail.com>
2024-06-20 16:46:52 +0200

    feat: update Specter adapter logic for Hashi

--- packages/evm/contracts/adapters/Spectre/SpectreAdapter.sol
+++ packages/evm/contracts/adapters/Spectre/SpectreAdapter.sol
@@ -1,30 +1,35 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.20;
 
+import { Merkle } from "../Electron/lib/Merkle.sol";
+import { Receipt } from "../Electron/lib/Receipt.sol";
+import { BlockHashAdapter } from "../BlockHashAdapter.sol";
 import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
-import "../BlockHashAdapter.sol";
+import { ISpectre } from "./interfaces/ISpectre.sol";
 
 contract SpectreAdapter is AccessControl, BlockHashAdapter {
     string public constant PROVIDER = "spectre";
 
-    struct Reporter {
-        uint128 chainID;
-        bool enabled;
-    }
+    // keccak256("MessageDispatched(uint256,(uint256,uint256,uint256,address,address,bytes,address[],address[]))")
+    bytes32 internal constant MESSAGE_DISPATCHED_EVENT_SIG =
+        0x218247aabc759e65b5bb92ccc074f9d62cd187259f2a0984c3c9cf91f67ff7cf;
 
-    address public immutable _handler;
-    mapping(address => Reporter) public reporters;
+    address public immutable SOURCE_YAHO;
+    uint256 public immutable SOURCE_CHAIN_ID;
+    address public spectreAddress;
 
-    error ArrayLengthMismatch();
     error Unauthorized();
-    error InvalidHandler(address handler);
-    error InvalidReporter(address reporter);
+    error InvalidEventSource();
+    error InvalidReceiptsRoot();
+    error ErrorParseReceipt();
+    error InvalidEventSignature();
+    error BlockHeaderRootMissing();
 
-    event ReporterSet(address reporterAddress, uint256 chainID, bool enabled);
-
-    constructor(address handler) {
-        _handler = handler;
+    constructor(address initialSpectreAddress, uint256 sourceChainId, address sourceYaho) {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        SOURCE_CHAIN_ID = sourceChainId;
+        SOURCE_YAHO = sourceYaho;
+        spectreAddress = initialSpectreAddress;
     }
 
     modifier onlyAdmin() {
@@ -32,17 +37,45 @@ contract SpectreAdapter is AccessControl, BlockHashAdapter {
         _;
     }
 
-    function setReporter(address reporterAddress, uint128 chainID, bool enabled) public onlyAdmin {
-        reporters[reporterAddress] = Reporter(chainID, enabled);
-        emit ReporterSet(reporterAddress, chainID, enabled);
+    function changeSpectreAddress(address newSpectreAddress) external onlyAdmin {
+        spectreAddress = newSpectreAddress;
     }
 
-    function storeHashes(address reporterAddress, uint256[] calldata ids, bytes32[] calldata hashes) public {
-        if (ids.length != hashes.length) revert ArrayLengthMismatch();
-        if (msg.sender != _handler) revert InvalidHandler(msg.sender);
-        Reporter memory reporter = reporters[reporterAddress];
-        if (!reporter.enabled) revert InvalidReporter(reporterAddress);
-        uint256 chainID = uint256(reporter.chainID);
-        _storeHashes(chainID, ids, hashes);
+    function verifyAndStoreDispatchedMessage(
+        uint64 srcSlot,
+        uint64 txSlot,
+        bytes32[] memory receiptsRootProof,
+        bytes32 receiptsRoot,
+        bytes[] memory receiptProof,
+        bytes memory txIndexRLPEncoded,
+        uint256 logIndex
+    ) external {
+        bytes32 blockHeaderRoot = ISpectre(spectreAddress).blockHeaderRoots(srcSlot);
+        if (blockHeaderRoot == bytes32(0)) revert BlockHeaderRootMissing();
+
+        bool isValidReceiptsRoot = Merkle.verifyReceiptsRoot(
+            receiptsRootProof,
+            receiptsRoot,
+            srcSlot,
+            txSlot,
+            blockHeaderRoot
+        );
+        if (!isValidReceiptsRoot) revert InvalidReceiptsRoot();
+
+        Receipt.ParsedReceipt memory parsedReceipt = Receipt.parseReceipt(
+            receiptsRoot,
+            receiptProof,
+            txIndexRLPEncoded,
+            logIndex
+        );
+
+        if (!parsedReceipt.isValid) revert ErrorParseReceipt();
+        if (bytes32(parsedReceipt.topics[0]) != MESSAGE_DISPATCHED_EVENT_SIG) revert InvalidEventSignature();
+        if (parsedReceipt.eventSource != SOURCE_YAHO) revert InvalidEventSource();
+
+        uint256 messageId = uint256(parsedReceipt.topics[1]);
+        bytes32 messageHash = keccak256(parsedReceipt.data);
+
+        _storeHash(SOURCE_CHAIN_ID, messageId, messageHash);
     }
 }
--- packages/evm/contracts/adapters/Spectre/interfaces/ISpectreAdapter.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.20;
-
-interface ISpectreAdapter {
-    function storeHashes(address reporter, uint256[] memory ids, bytes32[] memory _hashes) external;
-}
