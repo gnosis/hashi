c57a4ee76fb2a51429dbedc3c24cfd4bdd3cf44f
Alessandro Manfredi <alle.manfredi@gmail.com>
2024-09-20 08:07:34 +0200

    fix(reporter): adds nonce within simulateContract to avoid to generate txs with the same nonce

--- packages/reporter/src/controllers/StandardReporterController.ts
+++ packages/reporter/src/controllers/StandardReporterController.ts
@@ -24,6 +24,7 @@ class StandardReporterController extends BaseController {
       const client = this.multiClient.getClientByChain(this.sourceChain)
       const blockNumber = _blockNumbers[_blockNumbers.length - 1]
 
+      let nonce = await client.getTransactionCount({ address: client.account.address })
       for (const chain of this.destinationChains as Chain[]) {
         if (!this.adapterAddresses[chain.name]) {
           this.logger.info(`Adapter address is missing for ${chain.name}. Skipping...`)
@@ -37,10 +38,12 @@ class StandardReporterController extends BaseController {
           functionName: "dispatchBlocks",
           args: [chain.id, this.adapterAddresses[chain.name], [blockNumber]],
           value: this._reportHeadersValue,
+          nonce,
         })
 
         const txHash = await client.writeContract(request)
         this.logger.info(`headers reporter from ${this.sourceChain.name} to ${chain.name}: ${txHash}`)
+        nonce += 1
       }
     } catch (_error) {
       this.logger.error(_error)
