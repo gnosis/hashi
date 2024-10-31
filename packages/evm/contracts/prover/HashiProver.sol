2c98ae55ba7e17bf3b0d4a6bf1ef1b81ab9c626c
Alessandro Manfredi <alle.manfredi@gmail.com>
2024-10-31 16:31:42 +0100

    refactor(evm): adds comments to HashiProver fxs

--- packages/evm/contracts/prover/HashiProver.sol
+++ packages/evm/contracts/prover/HashiProver.sol
@@ -17,6 +17,21 @@ contract HashiProver is IHashiProver {
         SHOYU_BASHI = shoyuBashi;
     }
 
+    /**
+     * @dev Verifies and retrieves a specific event from a transaction receipt in a foreign blockchain.
+     *
+     * @param proof A `ReceiptProof` struct containing proof details:
+     * - chainId: The chain ID of the foreign blockchain.
+     * - blockNumber: If ancestralBlockNumber is 0, then blockNumber represents the block where the transaction occurred and is available in Hashi.
+     * - blockHeader: The header of the specified block.
+     * - ancestralBlockNumber: If provided, this is the block number where the transaction took place. In this case, blockNumber is the block whose header is accessible in Hashi.
+     * - ancestralBlockHeaders: Array of block headers to prove the ancestry of the specified block.
+     * - receiptProof: Proof data for locating the receipt in the Merkle Trie.
+     * - transactionIndex: Index of the transaction within the block.
+     * - logIndex: The specific log index within the transaction receipt.
+     *
+     * @return bytes The RLP-encoded event corresponding to the specified `logIndex`.
+     */
     function verifyForeignEvent(ReceiptProof calldata proof) internal view returns (bytes memory) {
         bytes memory blockHeader = _checkBlockHeaderAgainstHashi(
             proof.chainId,
@@ -37,6 +52,23 @@ contract HashiProver is IHashiProver {
         return logs[proof.logIndex].toRlpBytes();
     }
 
+    /**
+     * @dev Verifies foreign storage data for a specified account on a foreign blockchain.
+     *
+     * @param proof An `AccountAndStorageProof` struct containing proof details:
+     * - chainId: The chain ID of the foreign blockchain.
+     * - blockNumber: If ancestralBlockNumber is 0, then blockNumber represents the block where the transaction occurred and is available in Hashi.
+     * - blockHeader: The header of the specified block.
+     * - ancestralBlockNumber: If provided, this is the block number where the transaction took place. In this case, blockNumber is the block whose header is accessible in Hashi.
+     * - ancestralBlockHeaders: Array of block headers proving ancestry of the specified block.
+     * - account: The account address whose storage is being verified.
+     * - accountProof: Proof data for locating the account in the state trie.
+     * - storageHash: Expected hash of the storage root for the account.
+     * - storageKeys: Array of storage keys for which data is being verified.
+     * - storageProof: Proof data for locating the storage values in the storage trie.
+     *
+     * @return bytes[] An array of storage values corresponding to the specified `storageKeys`.
+     */
     function verifyForeignStorage(AccountAndStorageProof calldata proof) internal view returns (bytes[] memory) {
         bytes memory blockHeader = _checkBlockHeaderAgainstHashi(
             proof.chainId,
